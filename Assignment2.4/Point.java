009c7ae3dd243f2e18b081a125ac5a853e9c00f5
Azriy-Nazariee <1211104288@student.mmu.edu.my>
2024-01-24 23:18:37 +0800

    Refactor canMove method in Point class to allow moving forward by 1 or 2 squares

--- Assignment2.4/Point.java
+++ Assignment2.4/Point.java
@@ -13,17 +13,22 @@ public class Point extends Piece {
 
     public boolean canMove(int targetCol, int targetRow){
         if (isWithinBoard(targetCol, targetRow) && !isSameSquare(targetCol, targetRow)) {
-            // Move forward by 1 square (depending on the color)
+            // Move forward by 1 or 2 square(s) (depending on the color)
             int rowDifference = targetRow - preRow;
-            if (color == GamePanel.YELLOW && rowDifference == -1) {
+            int colDifference = targetCol - preCol;
+            if (color == GamePanel.YELLOW && (rowDifference == -1 || rowDifference == -2)) {
                 // Yellow pieces move towards the top of the board
-                if (Math.abs(targetCol - preCol) + Math.abs(rowDifference) == 1 && isValidSquare(targetCol, targetRow)) {
-                    return true;
+                if (Math.abs(rowDifference) == 1 || Math.abs(rowDifference) == 2) {
+                    if (Math.abs(colDifference) == 0 && isValidSquare(targetCol, targetRow) && pieceOnStraightLine(targetCol, targetRow)==false){
+                        return true;
+                    }
                 }
-            } else if (color == GamePanel.BLUE && rowDifference == 1) {
+            } else if (color == GamePanel.BLUE && (rowDifference == 1 || rowDifference == 2)) {
                 // Blue pieces move towards the bottom of the board
-                if (Math.abs(targetCol - preCol) + Math.abs(rowDifference) == 1 && isValidSquare(targetCol, targetRow)) {
-                    return true;
+                if (Math.abs(rowDifference) == 1 || Math.abs(rowDifference) == 2) {
+                    if (Math.abs(colDifference) == 0 && isValidSquare(targetCol, targetRow) && pieceOnStraightLine(targetCol, targetRow)==false){
+                        return true;
+                    }
                 }
             }
         }
