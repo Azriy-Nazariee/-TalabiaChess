04212d9242e1b3832ffe464f07e143ae038c64db
Azriy-Nazariee <1211104288@student.mmu.edu.my>
2024-01-25 01:45:35 +0800

    Add isReversed flag to Point class for handling reversed movement

--- Assignment2.4/Point.java
+++ Assignment2.4/Point.java
@@ -1,4 +1,7 @@
 public class Point extends Piece {
+
+    private boolean isReversed = false;
+    
     public Point(int color, int col, int row){
         super(color, col, row);
 
@@ -12,19 +15,18 @@ public class Point extends Piece {
     }
 
     public boolean canMove(int targetCol, int targetRow){
+        if ((color == GamePanel.YELLOW && preRow == 0) || (color == GamePanel.BLUE && preRow == 5)) {
+            isReversed = true;
+        }
+    
         if (isWithinBoard(targetCol, targetRow) && !isSameSquare(targetCol, targetRow)) {
             // Move forward by 1 or 2 square(s) (depending on the color)
             int rowDifference = targetRow - preRow;
             int colDifference = targetCol - preCol;
-            if (color == GamePanel.YELLOW && (rowDifference == -1 || rowDifference == -2)) {
-                // Yellow pieces move towards the top of the board
-                if (Math.abs(rowDifference) == 1 || Math.abs(rowDifference) == 2) {
-                    if (Math.abs(colDifference) == 0 && isValidSquare(targetCol, targetRow) && pieceOnStraightLine(targetCol, targetRow)==false){
-                        return true;
-                    }
-                }
-            } else if (color == GamePanel.BLUE && (rowDifference == 1 || rowDifference == 2)) {
-                // Blue pieces move towards the bottom of the board
+            if ((color == GamePanel.YELLOW && !isReversed && (rowDifference == -1 || rowDifference == -2)) || 
+                (color == GamePanel.YELLOW && isReversed && (rowDifference == 1 || rowDifference == 2)) ||
+                (color == GamePanel.BLUE && !isReversed && (rowDifference == 1 || rowDifference == 2)) ||
+                (color == GamePanel.BLUE && isReversed && (rowDifference == -1 || rowDifference == -2))) {
                 if (Math.abs(rowDifference) == 1 || Math.abs(rowDifference) == 2) {
                     if (Math.abs(colDifference) == 0 && isValidSquare(targetCol, targetRow) && pieceOnStraightLine(targetCol, targetRow)==false){
                         return true;
@@ -32,7 +34,7 @@ public class Point extends Piece {
                 }
             }
         }
-
+    
         return false;
     }
 }
