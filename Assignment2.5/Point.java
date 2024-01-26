998f7254fd38531814bf17a830e66b68d218c696
Azriy-Nazariee <1211104288@student.mmu.edu.my>
2024-01-27 00:59:11 +0800

    Fix the image orientation when they flipped

--- Assignment2.5/Point.java
+++ Assignment2.5/Point.java
@@ -1,26 +1,22 @@
 public class Point extends Piece {
 
-    public Point(int color, int col, int row,boolean isReversed, boolean isFlipBoard) {
+    public Point(int color, int col, int row, boolean isReversed, boolean isFlipBoard) {
         super(color, col, row);
         this.isReversed = isReversed;
-        if(isFlipBoard){
-            if (color == GamePanel.YELLOW) {
-                image = getImage("res/YPointR");
-            }
     
-            else {
-                image = getImage("res/BPointR");
-            }
-        } else{
+        if (isFlipBoard) {
             if (color == GamePanel.YELLOW) {
-                image = getImage("res/YPoint");
+                image = getImage(isReversed ? "res/YPoint" : "res/YPointR");
+            } else {
+                image = getImage(isReversed ? "res/BPoint" : "res/BPointR");
             }
-    
-            else {
-                image = getImage("res/BPoint");
+        } else {
+            if (color == GamePanel.YELLOW) {
+                image = getImage(isReversed ? "res/YPointR" : "res/YPoint");
+            } else {
+                image = getImage(isReversed ? "res/BPointR" : "res/BPoint");
             }
         }
-        
     }
 
     public boolean canMove(int targetCol, int targetRow) {
@@ -28,7 +24,7 @@ public class Point extends Piece {
             || (color == GamePanel.BLUE && preRow == 0) || (color == GamePanel.BLUE && preRow == 5)) {
             isReversed = true;
         }
-        
+
         if (isWithinBoard(targetCol, targetRow) && !isSameSquare(targetCol, targetRow) && !flipped) {
             // Move forward by 1 or 2 square(s) (depending on the color)
             int rowDifference = targetRow - preRow;
@@ -64,5 +60,13 @@ public class Point extends Piece {
         }
 
         return false;
+    }
+
+    private boolean flippedtoNormal(int targetCol, int targetRow) {
+        //check if the point returns to the original side's end of the board
+        if ((color == GamePanel.YELLOW && targetRow == 0) ||  (color == GamePanel.BLUE && targetRow == 5)) {
+            return false; // the point is no longer reversed, hence the isReversed is false
+        }
+        return true; //the point is still reversed, hence the isReversed is true
     }
 }
